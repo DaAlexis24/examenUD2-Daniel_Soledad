b55a7083facf30a04de8fc80177862702931f28d
danielSoledad <danielalesole24@gmail.com>
2025-03-20 13:16:59 +0100

    Update server1 with the respective libraries and imports

--- prototipo/src/server1.ts
+++ prototipo/src/server1.ts
@@ -1,6 +1,18 @@
-import express from 'express';
+import express from 'express'; // importamos el framework
+import type { Request, Response } from 'express'; //Importamos los tipos req y res que nos ayudarán a construir las funciones que configuraran nuestros endpoints
+import { v4 as uuidv4 } from 'uuid'; // Esto nos ayudará a crear ids en formato uuid a la hora de crear nuevos datos
+import { mockProducts } from './mock'; // Importamos nuestros datos iniciales
+import createDebug from 'debug'; // Esto nos ayudará a enseñar mensajes personalizados en nuestra consola
 
-const app = express();
+const app = express(); // Creamos la instancia de Express
+const debug = createDebug('products:api'); // Instanciamos debug
+
+debug('Iniciando API REST - Products-powered by DS-Network'); // mensaje de inicio
+app.disable('x-powered-by'); // Esto lo realizamos para ocultar la cabecera de Express cada vez que se realiza una petición a nuestra API mediante HTTP
+
+app.use(express.json()); // Este middleware nos permite parsear las peticiones que realizaremos con POSTMAN como si fueran un JSON
+
+let products = [...mockProducts];
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
